5fe15b128f51c077655397e63f53e822bbb4d6d9
albim72 <marcin.albiniak72@gmail.com>
2022-11-07 11:01:04 +0100

    Create podstawy.py

---
+++ DZIEN_1/podstawy.py
@@ -0,0 +1,31 @@
+print("to jest pierwsza linia kodu...")
+
+a = 7
+print(a)
+print(type(a))
+
+a = "siódemka"
+print(a)
+print(type(a))
+
+d:float
+d= 1.1
+
+print(d)
+print(type(d))
+
+d = True
+print(d)
+print(type(d))
+
+#komentarz w Pythonie
+"""
+komentarz dokumentacyjny 
+wieloliniowy
+"""
+
+#skrót CTRL+D -> powielenie linii lub bloku
+#skrót CTRL + / -> komentowanie/odkomentowanie bloku kodu
+
+
+
