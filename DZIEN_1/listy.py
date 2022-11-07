c8e039216c2ff3a3e906d609ed9de9b1cf162a79
albim72 <marcin.albiniak72@gmail.com>
2022-11-07 12:04:07 +0100

    Update listy.py

--- DZIEN_1/listy.py
+++ DZIEN_1/listy.py
@@ -12,9 +12,22 @@ print(kraj)
 kraj.insert(3,"Turcja")
 print(kraj)
 
-kraj.append(input("podaj nowy kraj... "))
-print(kraj)
+# kraj.append(input("podaj nowy kraj... "))
+# print(kraj)
 
 kraj.remove("Norwegia")
 print(kraj)
 print("Laos" in kraj)
+
+
+pies = ["buldog angielski","labrador","bokser"]
+pies_cena = [7000,5000,6100]
+
+sklepzoo = [[pies,"kot","papuga","mysz","szynszyla"],[pies_cena,2300,8700,45,320]]
+print(sklepzoo[0])
+print(sklepzoo[0][0][0],"kosztuje",sklepzoo[1][0][0],"zł")
+
+rasa = ["pitbull","york","spaniel"]
+
+rasa = rasa + pies
+print(rasa)
