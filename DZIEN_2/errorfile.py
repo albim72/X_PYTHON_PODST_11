a021515f31e50e253048bb32609d632cef39fa5a
albim72 <marcin.albiniak72@gmail.com>
2022-11-08 12:08:13 +0100

    Update errorfile.py

--- DZIEN_2/errorfile.py
+++ DZIEN_2/errorfile.py
@@ -4,14 +4,15 @@ try:
     f = open("waznedane.txt","r",encoding="utf-8")
     s = f.readline()
     i = int(s.strip())
-    raise Exception(d=i/0)
+    #raise Exception(d=i/0)
+    d = i/0
 except OSError as err:
     print(f"błąd systmowy: {err}")
 except ValueError:
     print("nie można przekonwertować danych ze str na int!")
-except Exception as ec:
-    print(f"jaki to błąd? {type(ec)}")
-    print(ec.args)
-    print(ec)
+# except Exception as ec:
+#     print(f"jaki to błąd? {type(ec)}")
+#     print(ec.args)
+#     print(ec)
 except:
     print(f"jaki to błąd? -> {sys.exc_info()[0]}")
