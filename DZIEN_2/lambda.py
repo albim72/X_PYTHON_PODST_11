904eab204ef3efaefbdd3fa5ed10686149739c2a
albim72 <marcin.albiniak72@gmail.com>
2022-11-08 10:12:02 +0100

    Update lambda.py

--- DZIEN_2/lambda.py
+++ DZIEN_2/lambda.py
@@ -17,3 +17,17 @@ def multip(h):
 print(multip(6)(3))
 
 nbs = [5,1,3,7,9,12,4,45,66,78,79,101,99,40,-45,-22,0]
+
+#stwórz nową listę przyste ulokuj w niej wartości parzyste z listy nbs
+#filter(funkcja,dane)
+#funkcja -> warunek filtrowania, dane-> dane do filtrowania
+#funkcja wyższego rzędu (filter) minimum jeden argument jest funckją
+
+parzysta = list(filter(lambda x:x%2 == 0,nbs))
+print(parzysta)
+
+def warunek(x):
+    return x%2 == 0
+
+parzysta = list(filter(warunek,nbs))
+print(parzysta)
