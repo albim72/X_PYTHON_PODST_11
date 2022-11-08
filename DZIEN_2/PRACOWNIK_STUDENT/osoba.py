3f8bda79ffad37fbfc0f83a4bfee6ede2f9357fb
albim72 <marcin.albiniak72@gmail.com>
2022-11-08 15:48:06 +0100

    Update osoba.py

--- DZIEN_2/PRACOWNIK_STUDENT/osoba.py
+++ DZIEN_2/PRACOWNIK_STUDENT/osoba.py
@@ -7,16 +7,29 @@ class Osoba:
         self.waga = waga
         self.kolor_oczu = "brązowy"
         self.info()
-        
+
     def info(self):
         print("powstał obiekt klasy Osoba....")
-        
+
     def print_osoba(self):
         print(f"osoba -> imię: {self.imie}, wiek: {self.wiek}, wzrost: {self.wzrost} cm, "
               f"waga: {self.waga} kg.")
-        
+
     def wiekza10lat(self):
         return self.wiek + 10
-    
+
     def czypracownik(self):
         return False
+    
+    def bmi(self):
+        return self.waga/(self.wzrost/100)**2
+    
+    def opis_bmi(self):
+        if self.bmi()<18.5:
+            return "niedowaga"
+        elif self.bmi()<=25:
+            return "waga prawidłowa"
+        elif self.bmi()<=30:
+            return "nadwaga"
+        else:
+            return "otyłość"
