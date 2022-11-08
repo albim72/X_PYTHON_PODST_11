e6506d4f2bcef0d5b5a75c04b588b1bff0397a4f
albim72 <marcin.albiniak72@gmail.com>
2022-11-08 15:31:57 +0100

    Update main.py

--- DZIEN_2/PRACOWNIK_STUDENT/main.py
+++ DZIEN_2/PRACOWNIK_STUDENT/main.py
@@ -1,5 +1,6 @@
 from osoba import Osoba
 from pracownik import Pracownik
+from student import Student
 
 os1 = Osoba("Jan",45,173,101)
 print(f"kolor oczu: {os1.kolor_oczu}")
@@ -23,3 +24,30 @@ pr1.print_osoba()
 pr1.print_pracownik()
 print(f"wiek za 10 lat: {pr1.wiekza10lat()}")
 print(f"czy osoba jest pracownikiem? ({pr1.czypracownik()})")
+
+print("__________________________________________________")
+
+st1 = Student("Olaf",22,178,88,34543534,"Automatyka,Elektronika i Informatyka","Informatyka",3)
+# st1.wynagrodzenie = 3000
+#
+# print(f"wynagrodzenie: {st1.wynagrodzenie}")
+st1.print_osoba()
+st1.print_student()
+print(f"wiek za 10 lat: {st1.wiekza10lat()}")
+print(f"czy osoba jest pracownikiem? ({st1.czypracownik()})")
+
+print("__________________________________________________")
+
+st2 = Student("Olga",23,170,58,978678,"Budowlany","Budowa mostów",4,"XYZ","sekretarka",1)
+# st1.wynagrodzenie = 3000
+#
+# print(f"wynagrodzenie: {st1.wynagrodzenie}")
+st2.print_osoba()
+st2.print_student()
+
+st2.print_pracownik()
+print(f"wiek za 10 lat: {st2.wiekza10lat()}")
+print(f"czy osoba jest pracownikiem? ({st2.czypracownik()})")
+
+#stwórz instancję  klasy student st3 -> student który nie jest pracownikiem, ale jest sportowcem
+# wyświetl wszytkie dostępne informacje o tym studencie, korzystając ze stosownych metod!
