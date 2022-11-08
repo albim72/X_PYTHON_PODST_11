b84dda2bdb6e675d08b90c8e287e64137df31d32
albim72 <marcin.albiniak72@gmail.com>
2022-11-08 15:25:32 +0100

    Update student.py

--- DZIEN_2/PRACOWNIK_STUDENT/student.py
+++ DZIEN_2/PRACOWNIK_STUDENT/student.py
@@ -6,11 +6,25 @@ class Student(Pracownik,Sport,Ekstra):
 
     #konstruktor z wielodziedziczeniem
     def __init__(self,imie,wiek,wzrost,waga,nr_studenta,wydzial,kierunek,rokstud,
-                 firma,stanowisko,latapracy,dyscyplina,lata_upr,best_wynik):
-        Pracownik.__init__(self,imie,wiek,wzrost,waga,firma,stanowisko,latapracy)
+                 firma="",stanowisko="",latapracy="",dyscyplina="",lata_upr="",best_wynik=""):
+        Pracownik.__init__(self,imie,wiek,wzrost,waga,firma,stanowisko,latapracy,0)
         Sport.__init__(self,dyscyplina,lata_upr,best_wynik)
         self.nr_studenta = nr_studenta
         self.wydzial = wydzial
         self.kierunek = kierunek
         self.rokstud = rokstud
+
+    # def __init__(self,nr_studenta,wydzial,kierunek,rokstud):
+    #     self.nr_studenta = nr_studenta
+    #     self.wydzial = wydzial
+    #     self.kierunek = kierunek
+    #     self.rokstud = rokstud
+    
+    def print_student(self):
+        print(f"dane studenta -> id: {self.nr_studenta}, wydział: {self.wydzial}, kierunek: {self.kierunek}, "
+              f"rok studiów: {self.rokstud}")
+
+    def czypracownik(self):
+        return self.firma != ""
         
+    
