d4ee8732f62c1d703c70e5b21695877d0b79d2e1
albim72 <marcin.albiniak72@gmail.com>
2022-11-08 14:43:22 +0100

    Create pracownik.py

---
+++ DZIEN_2/PRACOWNIK_STUDENT/pracownik.py
@@ -0,0 +1,14 @@
+from osoba import Osoba
+
+class Pracownik(Osoba):
+    #konstruktor z dziedziczeniem
+    def __init__(self,imie,wiek,wzrost,waga,firma,stanowisko,latapracy,wynagrodzenie):
+        super().__init__(imie,wiek,wzrost,waga)
+        self.firma = firma
+        self.stanowisko = stanowisko
+        self.latapracy = latapracy
+        self.wynagrodzenie = wynagrodzenie
+        
+    def print_pracownik(self):
+        print(f"dane pracownika -> firma: {self.firma}, stanowisko pracy: {self.stanowisko} , "
+              f"lata pracy: {self.latapracy}, wynagrodzenie: {self.wynagrodzenie} zł.")
