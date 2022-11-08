bad81b62ad639c2404d955f4163feee020c3af2a
albim72 <marcin.albiniak72@gmail.com>
2022-11-08 13:56:04 +0100

    Update mian.py

--- DZIEN_2/MODULY/mian.py
+++ DZIEN_2/MODULY/mian.py
@@ -2,9 +2,21 @@
 #import dane as dn
 from dane import book,stany_mag
 
+from mylib.specdef.kolfunkcje import nowymagazyn,nel_dict
+
 print(stany_mag)
 print(book)
 
 print(f"książka: {book['tytul']}, autor: {book['autor']}")
 print("magazyn nr 2")
 print(f"aktualna liczna książek w magazynie po zakupie: {stany_mag[1] -1}")
+
+lst = list(stany_mag)
+nowymagazyn(lst,50)
+
+print(lst)
+
+bk = dict(book)
+nel_dict(bk,"lakier",True)
+
+print(bk)
