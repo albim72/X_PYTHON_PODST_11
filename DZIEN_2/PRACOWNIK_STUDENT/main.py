f1fdda605662132cf51e08adc2f369ccf462d6f4
albim72 <marcin.albiniak72@gmail.com>
2022-11-08 14:49:33 +0100

    Update main.py

--- DZIEN_2/PRACOWNIK_STUDENT/main.py
+++ DZIEN_2/PRACOWNIK_STUDENT/main.py
@@ -1,4 +1,5 @@
 from osoba import Osoba
+from pracownik import Pracownik
 
 os1 = Osoba("Jan",45,173,101)
 print(f"kolor oczu: {os1.kolor_oczu}")
@@ -14,3 +15,11 @@ print(f"kolor oczu: {os2.kolor_oczu}")
 os2.print_osoba()
 print(f"wiek za 10 lat: {os2.wiekza10lat()}")
 print(f"czy osoba jest pracownikiem? ({os2.czypracownik()})")
+
+print("__________________________________________________")
+
+pr1 = Pracownik("Olaf",55,176,98,"ABC","dyrektor",10,9800)
+pr1.print_osoba()
+pr1.print_pracownik()
+print(f"wiek za 10 lat: {pr1.wiekza10lat()}")
+print(f"czy osoba jest pracownikiem? ({pr1.czypracownik()})")
