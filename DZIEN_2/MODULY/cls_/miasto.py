c9c920b15a59b1962defda4e281eef358c83579a
albim72 <marcin.albiniak72@gmail.com>
2022-11-08 13:59:04 +0100

    Create miasto.py

---
+++ DZIEN_2/MODULY/cls_/miasto.py
@@ -0,0 +1,8 @@
+class Miasto:
+    def __init__(self,id,nazwa,woj):
+        self.id = id
+        self.nazwa = nazwa
+        self.woj = woj
+        
+    def print_miasto(self):
+        print(f"miasto: {self.nazwa}, id: {self.id}, województwo: {self.woj}")
