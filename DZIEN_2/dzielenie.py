89afa2ff80403bb665b12d472111c22e28476086
albim72 <marcin.albiniak72@gmail.com>
2022-11-08 11:57:50 +0100

    Update dzielenie.py

--- DZIEN_2/dzielenie.py
+++ DZIEN_2/dzielenie.py
@@ -5,7 +5,22 @@ def podziel(x,y):
         print("dzielenie przez 0!")
     except NameError:
         print("brak danych")
+    except TypeError:
+        print("używaj do działania tylko liczby a nie tekst!")
     else:
         print(f"wynik działania: {wynik}")
     finally:
         print("policzmy coś jeszcze.....")
+
+try:
+    podziel(1,2)
+    podziel(2,1)
+    podziel(0.5563,23.9878)
+    podziel(True,4)
+    podziel(-0.000004,343243)
+    podziel(4,False)
+    podziel(4,"gjgsdjf")
+    podziel(5)
+except TypeError:
+    print("zbyt mało argumentów, muszą być 2!")
+
