3ae19e295904247b0658db6c63520dbe347a62bd
albim72 <marcin.albiniak72@gmail.com>
2022-11-09 08:10:12 +0100

    Create klasa123.py

---
+++ DZIEN_3/klasa123.py
@@ -0,0 +1,9 @@
+class PierwszaKlasa:
+
+    g = 9.99
+    def __init__(self,a:int,b:int):
+        self.a=a
+        self.b=b
+
+    def print_ab(self):
+        print(f"a = {self.a}, b = {self.b}, stała g = {self.g}")
