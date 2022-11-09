b15037a373458133a7d962fb68a4ef6dd037c852
albim72 <marcin.albiniak72@gmail.com>
2022-11-09 08:13:16 +0100

    Update klasa123.py

--- DZIEN_3/klasa123.py
+++ DZIEN_3/klasa123.py
@@ -7,3 +7,15 @@ class PierwszaKlasa:
 
     def print_ab(self):
         print(f"a = {self.a}, b = {self.b}, stała g = {self.g}")
+
+        
+class DrugaKlasa(PierwszaKlasa):
+    def __init__(self,a,b,c):
+        super().__init__(a,b)
+        self.c=c
+        
+    def print_abc(self):
+        print(f"a = {self.a}, b = {self.b}, c = {self.c}, stała g = {self.g}")
+        
+    def sumuj(self):
+        return self.a+self.b+self.c
