83ec88468f70a978b262257bb3f9eacdf1ebffba
albim72 <marcin.albiniak72@gmail.com>
2022-11-09 08:50:38 +0100

    Update klasa123.py

--- DZIEN_3/klasa123.py
+++ DZIEN_3/klasa123.py
@@ -2,69 +2,80 @@ import math
 
 
 class PierwszaKlasa:
-    g = 9.99
-    def __init__(self,a:int,b:int):
-        self.a=a
-        self.b=b
+    gs = 9.99
+
+    def __init__(self, a: int, b: int):
+        self.a = a
+        self.b = b
 
     def print_ab(self):
-        print(f"a = {self.a}, b = {self.b}, stała g = {self.g}")
+        print(f"a = {self.a}, b = {self.b}, stała g = {self.gs}")
 
 
 class DrugaKlasa(PierwszaKlasa):
-    def __init__(self,a,b,c):
-        super().__init__(a,b)
-        self.c=c
+    def __init__(self, a, b, c):
+        super().__init__(a, b)
+        self.c = c
 
     def print_abc(self):
-        print(f"a = {self.a}, b = {self.b}, c = {self.c}, stała g = {self.g}")
+        print(f"a = {self.a}, b = {self.b}, c = {self.c}, stała g = {self.gs}")
 
     def sumuj(self):
-        return self.a+self.b+self.c
+        return self.a + self.b + self.c
+
 
 class Pierwiastek:
-    def pierw_z_sumy(self,suma):
+    @staticmethod
+    def pierw_z_sumy(suma):
         return math.sqrt(suma)
-    
+
+
 class Potrojenie:
-    
-    def __init__(self,g,h):
-        self.g = g+2
-        self.h = h*3
-        
+
+    def __init__(self, g, h):
+        self.g = g + 2
+        self.h = h * 3
+
     def func_gh(self):
-        return self.g*self.h
-        
-#zadanie: Rozbuduj klasę TrzeciaKlasa o dziedziczenie dodatkowo klas Pierwiastek i Potrojenie
-#zmodyfikuj konstruktor klasy
-#przepuduj metody:
-#1. print_abcd() -> print_abcdgh() -> dodaj wyświetlanie wartości g i h
-#2. sumuj() -> a+b+c+d+func_gh()
-#Uzupełnij stworzony obiekt tk tak aby uwzględnić zmiany
+        return self.g * self.h
 
-class TrzeciaKlasa(DrugaKlasa):
-    def __init__(self,a,b,c,d):
-        super().__init__(a,b,c)
-        self.d=d
 
-    def print_abcd(self):
-        print(f"a = {self.a}, b = {self.b}, c = {self.c}, d = {self.d}, stała g = {self.g}")
+# zadanie: Rozbuduj klasę TrzeciaKlasa o dziedziczenie dodatkowo klas Pierwiastek i Potrojenie
+# zmodyfikuj konstruktor klasy
+# przepuduj metody:
+# 1. print_abcd() -> print_abcdgh() -> dodaj wyświetlanie wartości g i h
+# 2. sumuj() -> a+b+c+d+func_gh()
+# Uzupełnij stworzony obiekt tk tak aby uwzględnić zmiany
+
+class TrzeciaKlasa(DrugaKlasa, Pierwiastek, Potrojenie):
+    def __init__(self, a, b, c, d, g, h):
+        DrugaKlasa.__init__(self, a, b, c)
+        Potrojenie.__init__(self, g, h)
+        self.d = d
+
+    def print_abcdgh(self):
+        print(f"a = {self.a}, b = {self.b}, c = {self.c}, d = {self.d}, "
+              f"g = {self.g}, h = {self.h}, stała g = {self.gs}")
 
     def sumuj(self):
-        return self.a+self.b+self.c+self.d
+        return self.a + self.b + self.c + self.d + self.func_gh()
 
 
 print("_________ Klasa Pierwsza _____________")
-pk = PierwszaKlasa(4,7)
+pk = PierwszaKlasa(4, 7)
 pk.print_ab()
 print("_________ Klasa Druga _____________")
-dk =DrugaKlasa(5,2,8)
+dk = DrugaKlasa(5, 2, 8)
 dk.print_ab()
 dk.print_abc()
 print(f"suma wynosi: {dk.sumuj()}")
 print("_________ Klasa Trzecia _____________")
-tk = TrzeciaKlasa(7,5,3,9)
+tk = TrzeciaKlasa(7, 5, 3, 9, 3, 2)
 tk.print_ab()
 tk.print_abc()
-tk.print_abcd()
+tk.print_abcdgh()
 print(f"suma wynosi: {tk.sumuj()}")
+print(Pierwiastek.pierw_z_sumy(45))
+print(tk.pierw_z_sumy(tk.sumuj()))
+print(Pierwiastek.pierw_z_sumy(tk.sumuj()))
+print(Pierwiastek.pierw_z_sumy(6867))
